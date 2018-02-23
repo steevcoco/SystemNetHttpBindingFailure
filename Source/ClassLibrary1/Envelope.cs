b24de31dd5f9ec4b70e4e1ba430e3d870e498ad0
steevcoco <coco@stevencoco.com>
2018-02-23 10:45:12 -0500

    Commit files.

---
+++ Source/ClassLibrary1/Envelope.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ClassLibrary1
+{
+	[DataContract]
+	public class Envelope
+	{
+		public async Task<Envelope> PostAsync(
+				Uri uri,
+				HttpMessageInvoker httpClient,
+				CancellationToken cancellationToken = default)
+		{
+			return this;
+		}
+	}
+}
---
+++ Source/WpfApp2/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows;
+using ClassLibrary1;
+
+
+namespace WpfApp2
+{
+	public partial class App : Application
+	{
+		private void handleAppStartup(object sender, StartupEventArgs e)
+		{
+			Console.WriteLine("Hello World!");
+
+			Envelope envelope = new Envelope();
+			Console.WriteLine(
+					envelope.PostAsync(
+									new Uri("http://example.com"),
+									new HttpClient())
+							.Result);
+
+			Console.WriteLine("SUCCESS!");
+		}
+	}
+}
